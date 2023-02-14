12d4a64f9499de52c56c1e1db4b936767a69276f
Dardanz <dardonz@gmail.com>
2023-02-13 23:30:36 -0500

    Updated Javascript to geneerate X/O's and keep them static

--- tictac.js
+++ tictac.js
@@ -8,34 +8,60 @@ const player = (sign) => {
 
 
 const gameBoard = (() => {
-    const board = ["X", "O", "X",
-                   "O", "X", "O",
-                   "O", "O", "X"];
+    const board = ["", "", "",
+                   "", "", "",
+                   "", "", ""];
+
+    const setField = (position, playerSign) => {
+        board[position] = playerSign;
+    }
 
     const getField = (position) => {
         return board[position];
     };
 
-    return { getField };
+    return { getField, setField };
 })();
 
 
 const displayController = (() => {
     const individualFields = document.querySelectorAll(".field");
 
-    console.log("asdg");
-
     individualFields.forEach(
-        (field) => 
-            field.addEventListener("click", (e) => {
-                updateGameboard();
-            })
+        (field) =>             
+            field.addEventListener("click", (e) => 
+                {
+                    if (e.target.textContent !== "") return;
+                    gameController.playRound(e.target.dataset.index);
+                    updateGameboard();
+                }
+            )
     );
 
     const updateGameboard = () => {
         for (let i = 0; i < individualFields.length; i++) {
             individualFields[i].textContent = gameBoard.getField(i);
         }
-      };
+      };       
+      
+})();
 
+const gameController = (() => {
+    const playerOne = player("X");
+    const playerTwo = player("O");
+    let round = 1;
+    let gameOver = false;
+    
+    const playRound = (fieldPosition) => {
+        gameBoard.setField(fieldPosition, getCurrentPlayerSign());
+        round++;
+    }
+
+    const getCurrentPlayerSign = () => {
+        return round % 2 === 1 ? playerOne.getSign() : playerTwo.getSign();
+    };
+
+    
+
+    return { playRound };
 })();
